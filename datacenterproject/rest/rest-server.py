a919c7cb2194611af0a3d5de85dbbb69c18a8958
bvjhansisaketa <bvjhansisaketa@gmail.com>
2022-12-08 16:52:12 -0700

    Changes in website

--- datacenterproject/rest/rest-server.py
+++ datacenterproject/rest/rest-server.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/team.html')
+def team():
+    return render_template("team.html")
+
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
+
 if __name__=='__main__':
     # app.run()
     app.run(host="0.0.0.0", port=5000)
