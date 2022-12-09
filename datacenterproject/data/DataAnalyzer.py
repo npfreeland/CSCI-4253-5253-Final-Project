519ec7df0601ab002a4373dc06c4f159c2a54971
bvjhansisaketa <bvjhansisaketa@gmail.com>
2022-12-08 19:43:30 -0700

    changes

--- datacenterproject/data/DataAnalyzer.py
+++ datacenterproject/data/DataAnalyzer.py
@@ -9,36 +9,48 @@ from io import BytesIO
 import pandas as pd
 from nba_api.stats.endpoints import Player, Team, LeagueStandings, ScoreboardV2
 
+
+
+# set bucket names 
 queueBucketName = "inputs"
 outputBucketName = "outputs"
 
+
+# minio connection
 minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
 minioUser = os.getenv("MINIO_USER") or "rootuser"
 minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
 minioClient = Minio(minioHost,access_key = minioUser, secret_key=minioPasswd, secure=False)
+
+# RabbitMQ connection
 hostname= os.environ['RABBIT_HOST'] if 'RABBIT_HOST' in os.environ else 'localhost'
 
+# connecting RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host=hostname))
 channel = connection.channel()
 
+# Declare the queue
 channel.queue_declare(queue='DataQueue')
-from minio import Minio
-minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
-minioUser = os.getenv("MINIO_USER") or "rootuser"
-minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
-minioClient = Minio(minioHost,access_key = minioUser, secret_key=minioPasswd, secure=False)
+
 print( ' [*] Waiting for messages. To exit press CTRL+C' )
 print("DataQueue")
+
+
+# callback after receiving the data from the data collector 
 def callback(ch, method, properties, body):
-    # print( " [x] Received %r" % (body,) )
+    print( " [x] Received the data from Data collector")
+
+    # converting the data from json to list format 
     json_to_list = json.loads(body)
-    print(json_to_list[0])
+    # convert that into the dataframe
     df = pd.DataFrame(json_to_list)
     # Sending to Minio
     createBuckets()
+    # convert the dataframe to the csv
     csv = df.to_csv().encode('utf-8')
-    minioClient.put_object(queueBucketName,"Data.csv",data=BytesIO(csv),length=len(csv),content_type='application/csv')
+    # store it in the minio bucket
+    minioClient.put_object(queueBucketName,"/games/games.csv",data=BytesIO(csv),length=len(csv),content_type='application/csv')
 
 
 def createBuckets():
@@ -48,60 +60,10 @@ def createBuckets():
         minioClient.make_bucket(outputBucketName)
    
 
-
+# data consuming and calling the callback
 channel.basic_consume(queue='DataQueue', auto_ack=False, on_message_callback=callback)
 
 channel.start_consuming()
 
 
-# get the list of players currently in the NBA from the Minio server
-def getActivePlayers():
-    # Create a new bucket on the Minio server
-    # Retrieve the list of current NBA players
-    players = Player.get_active_players()
 
-    # Save the list of players as a JSON file on the Minio server
-    json_data = json.dumps(players)
-    minioClient.put_object('nba-data', 'players.json', json_data, 'application/json')
-
-    # Retrieve the list of players from the Minio server
-    try:
-        data = client.get_object('nba-data', 'players.json')
-        json_data = data.read()
-        players = json.loads(json_data)
-    except ResponseError as err:
-        print(err)
-
-# same can be done for other things like teams, standings, etc.
-
-# Retrieve the current year
-current_year = datetime.now().year
-
-# # Retrieve the game data for the last 10 years
-# for year in range(current_year - 10, current_year):
-#     # Retrieve the game data for the current year
-#     games = ScoreboardV2.ScoreboardV2(league_id='00', season=year).get_data_frames()
-
-#     # Save the game data as a .csv file on the Minio server
-#     csv_data = games.to_csv()
-#     client.put_object('nba-data', f'games_{year}.csv', csv_data, 'text/csv')
-
-# # Retrieve the game data for the current year from the Minio server
-# try:
-#     data = client.get_object('nba-data', f'games_{current_year}.csv')
-#     csv_data = data.read()
-#     games = pd.read_csv(StringIO(csv_data))
-# except ResponseError as err:
-#     print(err)
-
-# # Fetch player statistics and store them in the Minio bucket
-# player_stats = nba.player_stats()
-# minio_client.put_object('nba-statistics-bucket', 'player-statistics.json', player_stats)
-
-# # Fetch game schedules and store them in the Minio bucket
-# schedules = nba.schedule()
-# minio_client.put_object('nba-statistics-bucket', 'schedules.json', schedules)
-
-# # Fetch historical data and store it in the Minio bucket
-# historical_data = nba.historical()
-# minio_client.put_object('nba-statistics-bucket', 'historical-data.json', historical_data)
--- datacenterproject/data/DataCollector.py
+++ datacenterproject/data/DataCollector.py
@@ -1,59 +1,47 @@
 import pika
-
-import os
-hostname= os.environ['RABBIT_HOST'] if 'RABBIT_HOST' in os.environ else 'localhost'
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host=hostname))
-channel = connection.channel()
-
-channel.queue_declare(queue='DataQueue')
-
 import numpy as np
 import json
 import pandas as pd
 import matplotlib.pylab as plt
 import random as r
-# %matplotlib inline
-
 # pip install nba_api
 import nba_api as nba
 import nba_api.stats.endpoints as ep
+import os
 
 
-from nba_api.stats.static import teams
+# RabbitMQ connection
+hostname= os.environ['RABBIT_HOST'] if 'RABBIT_HOST' in os.environ else 'localhost'
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=hostname))
+channel = connection.channel()
 
-nba_teams = teams.get_teams()
-# Select the dictionary for the Celtics, which contains their team ID
-celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
-celtics_id = celtics['id']
+
+
+# Declare a queue for teams 
+channel.queue_declare(queue='DataQueue')
 
 from nba_api.stats.endpoints import leaguegamefinder
+
 
 # Query for games where the Celtics were playing
 gamefinder = leaguegamefinder.LeagueGameFinder(league_id_nullable='00')
 # The first DataFrame of those returned is what we want.
 games = gamefinder.get_data_frames()[0]
 games = games.dropna(how='any',axis=0)
-games = games[games.SEASON_ID.str[-4:] != '2022']
-games = games[games.SEASON_ID.str[-4:] != '2021']
 games.head(10)
 
-# print("games")
-print(type(games))
-# print(games.values.tolist())
-
-print(games.columns.values.tolist())
+# Convert the dataframe into the list 
 list_to_pass = [games.columns.values.tolist()] + games.values.tolist()
+
+# dump the list into the json format to send to the message queue
 json_string = json.dumps(list_to_pass)
-print(type(json_string))
-# print(json_string)
-print(list_to_pass[0:2])
+
 
 
 
 channel.basic_publish(exchange='',
                       routing_key='DataQueue',
                       body=json_string)
-print( " [x] Sent 'Hello World!'" )
+print( " [x] Sent the data to the Data Analyzer!!" )
 connection.close()
